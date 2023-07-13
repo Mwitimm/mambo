948a6c1066ba8a61e470b0af25ac8ef7424da5f5
Martin <mwendamarto7@gmail.com>
2023-07-13 16:43:20 +0300

    Changed the Source of the Navigation Bar

--- src/routes/twobed/+page.svelte
+++ src/routes/twobed/+page.svelte
@@ -1,6 +1,6 @@
 
 <script>
-    import Enav from "$lib/residence/Enav.svelte"
+    import Enav from "$lib/residence/twobed/Enav.svelte"
     import Grid from "$lib/residence/Grid.svelte"
     import Book from "$lib/residence/Book.svelte"
     import Ebook from "$lib/residence/Ebook.svelte"
